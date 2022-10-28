cb6e1f95aadddc3c43f828c6b5f306289635441d
NickovSH <nikita2000_30@mail.ru>
2022-10-28 12:01:23 +0700

    A new module Product of Digits has been added.

--- Product_of_digits.py
+++ Product_of_digits.py
@@ -6,13 +6,14 @@ def Product():
     digit = input()
     product_digit = 1
 
-    for i in digit:
-        if int(i) != 0:
-            product_digit = product_digit*int(i)
-        else:
-            product_digit
+    try:
+        for i in digit:
+            if int(i) != 0:
+                product_digit = product_digit*int(i)
+            else:
+                product_digit
+    except Exception:
+        print('You didn\'t enter a number, please try again')
 
     print(f'The product of the digits of the number {digit} is {product_digit}'.format(digit=digit, product_digit=product_digit))
 
-
-Product()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import Python_art
 import Header
 import Money_format
 import Rug_designer
+import Product_of_digits
 
 if __name__ == '__main__':
     Welcome_to_Python.Name_Surname()
@@ -10,4 +11,5 @@ if __name__ == '__main__':
     Header.Header()
     Money_format.Money_format()
     Rug_designer.Rug_designer()
+    Product_of_digits.Product()
 
